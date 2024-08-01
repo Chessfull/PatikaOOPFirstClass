72633204b175a45dc31ebbb0f989dcfb58ee34eb
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-01 11:05:25 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOOPFirstClass
+{
+    public class Person
+    {
+        private string _name;
+        private string _surName;
+        private string _birthDate;
+        public string status;
+
+
+        public string name
+        {
+            get { return name; }
+            set { _name = value; }
+        }
+
+        public string surName
+        {
+            get { return surName; }
+            set { _surName = value; }
+        }
+        public string birthDate
+        {
+            get { return birthDate; }
+            set { _birthDate = value; }
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Hi! My name is {_name} {_surName}.I born in {_birthDate}.  I am {status}. ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOOPFirstClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ▼ Instance and defining fields/properties of student sample ▼
+            Person person1 = new Person();
+            person1.name = "Mert";
+            person1.surName = "Topçu";
+            person1.birthDate = "17.01.1995";
+            person1.status = "Student";
+
+            // ▼ Instance and defining fields/properties of teacher sample ▼
+            Person person2 = new Person();
+            person2.name = "Sertan";
+            person2.surName = "Bozkuş";
+            person2.birthDate = "01.01.1993";
+            person2.status = "Teacher";
+
+            // ▼ Getting their informations with method in Person class 'Person.cs' ▼
+            person1.Greeting();
+            person2.Greeting();
+        }
+    }
+}
